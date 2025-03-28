3d882ec47bfcf12fc28da49b4de052cd9b6f471c
Jakie Khan <khanjakie651@gmail.com>
2025-03-28 22:43:50 +0545

    New

---
+++ challenge.js
@@ -0,0 +1,113 @@
+// // Challenge iterate over an array and add up all the Elements and return Total
+// // const arr = [22,33,44,55];
+
+
+// // Challenge 
+// // Iterate over and array and multiply all the elements in the array
+
+// // Challenge
+// var product = [ [1,2], [3,4] , [5,6,7] ];
+
+// product[0][0];
+// for(let i = 0;i < product.length;i++){
+//     for(let j = 0; j < product[i].length;j++){
+//         console.log(product[i][j])
+//         // console.log(i,j);
+//     }
+
+// }
+
+let arr=[[1,2],[3,4],[5,6,7]];
+
+for (let i=0;i<=arr.length-1;i++)
+{
+    let multiply=1*arr[i];
+    for(let j=0;j<=arr[arr.length-1];j++)
+    {
+        multiply=multiply*arr[i][j];
+        console.log(multiply);
+    }
+}
+
+// let arr=[22,33,44,55];
+
+// for (let i=0;i <=arr.length-1;i++)
+// {
+//     let sum=0;
+//     sum=sum+arr[i];
+//     console.log(sum);
+// }
+
+
+// // write a  function that generates a random number between a range
+
+// // function ourRange(max,min){
+// //     return Math.floor( Math.random() * (max - min + 1) + min )
+// // }
+
+// // console.log(ourRange(7,6));
+
+
+// // Coding Challenge
+
+
+// // 1. Print Numbers from 1 to 10
+// // Write a loop that prints numbers from 1 to 10 in the console. 
+
+
+// // 2. Print Even Numbers from 1 to 20
+// // Write a loop that prints only even numbers from 1 to 20.
+
+// // 3. Sum of First 10 Natural Numbers
+// // Calculate and print the sum of numbers from 1 to 10.
+
+
+// // 4. Reverse Counting from 10 to 1
+// // Print numbers from 10 to 1 in descending order.
+
+
+// // 5. Print Multiplication Table of 5
+// // Write a loop that prints the multiplication table of 5 up to 10.
+
+
+// // Operator Precedence
+// // Parentheses () have the highest precedence – always evaluated first.
+// // Exponentiation ** is evaluated before multiplication and division.
+// // Multiplication *, Division /, and Modulus % 
+// //  Addition + and Subtraction -
+
+// // console.log(5 + 3 * 2);   
+// // console.log((5 + 3) * 2);
+// // console.log(2 ** 3 * 2);   
+// // console.log(2 ** (3 * 2));  
+// // console.log((2 ** 3) ** 2);
+// // console.log(10 / 2 * 5);   
+// // console.log(10 % 3 + 2 * 4);  
+// // console.log((10 % (3 + 2)) * 4);
+// // console.log(10 + 5 * 2 ** 2);  
+// // console.log((10 + 5) * 2 ** 2);  
+// // console.log(10 + (5 * 2) ** 2);  
+// // console.log(20 / 2 + 3);  
+// // console.log(20 / (2 + 3));  
+// // console.log(20 / 2 * 3);  
+// // console.log((20 / 2) * 3);
+
+// // ##################################################################################################
+
+// // Roll a dice
+// // Create a function rollDice that return randrom number between 1 and 6;
+
+// // ##################################################################################################
+
+
+// // write a  function that generates a random number between a range. Try it yourself
+
+// // Solution 
+// // function ourRange(max,min){
+// //     return Math.floor( Math.random() * (max - min + 1) + min )
+// // }
+
+// // console.log(ourRange(7,4));
+
+// // ##################################################################################################
+
